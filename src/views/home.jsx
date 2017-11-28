e56be9f87d662d4660bdb59a280c7397753f86e0
Fleetwood Mac <me@Fleetwood-Mac.local>
2017-11-28 14:07:59 +1100

    Add grid for books and dummy markup to test layout

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const Home = () => (
-  <div className="row">
-    <div className="col-12 text-center">
-      <h1>Welcome!</h1>
+  <div className="l-container">
+    <div className="l-row">
+      <h1>Welcome Ian!</h1>
+    </div>
+    <div className="l-row">
+      <ul className="l-book-grid">
+        <li className="l-book-grid__item c-book">
+          <img
+            className="c-book__image"
+            src="https://lorempixel.com/640/480/?82539"
+            width="400"
+            height="300"
+            alt="Becker West Arnoldo"
+          />
+          <div className="c-book__details">
+            <ul className="c-details">
+              <li className="c-details__title">Title: Becker West Arnoldo</li>
+              <li className="c-details__author">Author: Mrs. John Doe</li>
+              <li className="c-details__date-published">Published: 2000-01-01</li>
+              <li className="c-details__isbn">ISBN: 3181444340</li>
+            </ul>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 );
