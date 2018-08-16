31b31de18c3fdab612dc7026c0817419eeba8908
Davide Gaido <davide.0plus1@gmail.com>
2018-08-16 14:39:10 +1000

    Update and reconfigure jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   verbose: true,
   setupFiles: ['<rootDir>/tests/shim.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
 };
