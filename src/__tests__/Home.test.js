6adc618131cfe34c3ed71000a96e44820ec90852
Jonathan Vanherpe <git@freebase.be>
2019-09-17 12:24:29 +1000

    make project work on case-sensitive filesystems

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Home from '../views/Home';
+import Home from '../views/home';
 
 describe('<Home />', () => {
   describe('renders', () => {
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,7 +5,7 @@ import {
   Switch,
 } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import Home from './views/Home';
+import Home from './views/home';
 
 const Routes = () => (
   <Router history={createBrowserHistory()}>
