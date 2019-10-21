52b52eed83399b7144406e93619e2cfba285a5d6
Joey <joybee210@gmail.com>
2019-10-21 20:08:50 +1100

    feat: add redux redux-saga redux-logger and react-router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import saga from './sagas';
+import reducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 import 'sanitize.css';
+import './css/reset.scss';
 
 import Routes from './routes';
 
+const middlewares = [];
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+middlewares.push(sagaMiddleware);
+// mount it on the Store
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(...middlewares),
+  ),
+);
+
+// then run the saga
+sagaMiddleware.run(saga);
+
+
+// render the application
 ReactDOM.render(
   // TODO use jsx extension for this file, will require to eject the create-react-app
   // eslint-disable-next-line react/jsx-filename-extension
-  <Routes />,
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
   document.getElementById('root'),
 );
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import {
-  Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import Home from './views/Home';
+import App from './containers/App';
 
 const Routes = () => (
   <Router history={createBrowserHistory()}>
     <Switch>
-      <Route exact path="/:testRouting?" component={Home} />
+      <Route path="/" component={App} />
     </Switch>
   </Router>
 );
