7eaa5443f4f0eae9522635207c2d562cdfdcfafb
David Quaglieri <david@product.fan>
2019-11-28 06:26:32 +1100

    add core MaterialUI componentry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import 'typeface-roboto';
 import 'sanitize.css';
 
 import Routes from './routes';
