5b662520aabe156a8f8f48117f92e23947ef9c81
David Quaglieri <david@product.fan>
2019-11-28 13:35:55 +1100

    add axios call. add camelcase-keys to transform response so object keys in response match airBnb style needs

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Layout from "./Layout";
+import { useAsync, IfPending, IfFulfilled, IfRejected } from "react-async";
+import axios from 'axios';
+import BookResults from "../components/BookResults";
+import camelcaseKeys from 'camelcase-keys';
 
 const HomeStyle = {
   display: 'flex',
@@ -8,18 +12,30 @@ const HomeStyle = {
   justifyContent: 'center',
 };
 
-const Home = ({ match }) => (
-  <div style={HomeStyle}>
-    <h1>
-      Welcome!
-    </h1>
-    {(match.params.testRouting) && (
-      <p>
-        {match.params.testRouting}
-      </p>
-    )}
-  </div>
-);
+const loadBooks = () => axios.get('https://my-json-server.typicode.com/0plus1/CodingChallenge-react/books')
+    .then((response) => {
+        return {
+            ...response,
+            data: camelcaseKeys(response.data)
+        };
+    });
+
+const Home = () => {
+  const state = useAsync({ promiseFn: loadBooks });
+
+  return (
+      <div style={HomeStyle}>
+        <h1>
+          Welcome!
+        </h1>
+        <IfPending state={state}>Loading...</IfPending>
+        <IfRejected state={state}>{error => `Something went wrong: ${error.message}`}</IfRejected>
+        <IfFulfilled state={state}>
+          {({data}) => <BookResults books={data}/>}
+        </IfFulfilled>
+      </div>
+  );
+};
 
 Home.propTypes = {
   match: PropTypes.shape({
